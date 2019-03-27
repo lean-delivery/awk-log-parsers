c8bf9e300af66518596fbb125de654e3244c1430
Timur Gadiev <Timur_Gadiev@epam.com>
2019-03-27 15:11:02 +0300

    Add separate jq install
    Signed-off-by: Timur Gadiev <Timur_Gadiev@epam.com>

--- test/test_inside_docker.sh
+++ test/test_inside_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cat /etc/centos-release
-yum install -y epel-release jq
+yum install -y epel-release
+yum install -y jq
 yum install -y https://bintray.com/api/ui/download/lean-delivery/gawk_extended/gawk_extended/gawk-4.2.1.31-1.el6.x86_64.rpm
 awk -V
 yum install -y https://bintray.com/api/ui/download/lean-delivery/gawk_extended/gawk-json-1.0.2-1.el6.noarch.rpm
