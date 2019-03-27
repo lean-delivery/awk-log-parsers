7da966d6467c96ec90f3edafc1a22edd997cd591
Timur Gadiev <Timur_Gadiev@epam.com>
2019-03-27 14:09:27 +0300

    Add gawk-json
    Signed-off-by: Timur Gadiev <Timur_Gadiev@epam.com>

--- test/test_inside_docker.sh
+++ test/test_inside_docker.sh
@@ -2,3 +2,4 @@
 cat /etc/centos-release
 yum install -y https://bintray.com/api/ui/download/lean-delivery/gawk_extended/gawk_extended/gawk-4.2.1.31-1.el6.x86_64.rpm
 awk -V
+yum install -y https://bintray.com/api/ui/download/lean-delivery/gawk_extended/gawk-json-1.0.2-1.el6.noarch.rpm
